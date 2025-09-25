aa074792dbd6fd912080b4016bc66c6e7f0a04a7
ynitylearn <ynitylearn@gmail.com>
2025-09-25 17:20:46 +0100

    logo5

--- components/SiteHeader.jsx
+++ components/SiteHeader.jsx
@@ -648,7 +648,7 @@ export default function SiteHeader({ mode = "public", onLogout }) {
               aria-label={t("logoAlt")}
             >
               <Image
-                src="/mtr_logo.png"
+                src="/logoMTR.png"
                 alt={t("logoAlt")}
                 width={200}
                 height={150}
