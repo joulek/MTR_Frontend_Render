b83160bddbe579608795b48a02afd24fa6859784
nourhene <nourheneabbes12@gmail.com>
2025-09-27 13:52:53 +0100

    devis

--- app/[locale]/admin/devis/page.jsx
+++ app/[locale]/admin/devis/page.jsx
@@ -14,7 +14,7 @@ import DevisGrilleList from "@/components/admin/devis/DevisGrilleList";
 // import DevisAllList from "@/components/admin/devis/DevisAllList"; // si tu en as une
 
 // ---- Nouvelle liste demandes ----
-import DemandeDevisList from "@/components/admin/devis";
+import DemandeDevisList from "@/components/admin/devis/DemandeDevisList";
 
 export default function AdminDevisSelector() {
   const t = useTranslations("auth.admin.devisAdmin");
