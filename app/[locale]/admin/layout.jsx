68bb955dec857a997a5a819eebf2c2cf6b5a5748
ynitylearn <ynitylearn@gmail.com>
2025-10-03 20:32:14 +0100

    precedents

--- app/[locale]/admin/layout.jsx
+++ app/[locale]/admin/layout.jsx
@@ -1,15 +1,19 @@
-// app/[locale]/admin/layout.jsx
+// @ts-nocheck
+
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import AdminLayoutClient from "./layout.client"; // <-- ton layout client renommé
+import AdminLayoutClient from "./layout.client";
 
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 
-export default function AdminLayout({ children, params: { locale } }) {
+export default function AdminLayout({ children, params }) {
+  const locale = params?.locale || "fr";
   const role = cookies().get("role")?.value || null;
+
   if (role !== "admin") {
     redirect(`/${locale}/login`);
   }
+
   return <AdminLayoutClient>{children}</AdminLayoutClient>;
 }
